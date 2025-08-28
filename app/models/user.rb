c4730c187e29df59de526dd04a9e25301615f67f
Elisson17 <elissoncordeiro2004@gmail.com>
2025-08-28 01:11:46 -0300

    Configurando modelo e tabela do user

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  def admin?
+    role == "admin"
+  end
+
+  def user?
+    role == "user"
+  end
+end
