aa6a754b3042b786df8193b94d66bb92a1b7638c
Elisson17 <elissoncordeiro2004@gmail.com>
2025-08-28 01:48:41 -0300

    Configurando rota de tela inicial

---
+++ app/controllers/v1/base_controller.rb
@@ -0,0 +1,3 @@
+class V1::BaseController < ApplicationController
+  # Common functionality for V1 controllers
+end
---
+++ app/controllers/v1/home_controller.rb
@@ -0,0 +1,5 @@
+class V1::HomeController < V1::BaseController
+  def index
+    # lógica para web
+  end
+end
