e5a53061673c65025407bccc4c2ed8642e913f8a
Elisson17 <elissoncordeiro2004@gmail.com>
2025-08-29 01:45:48 -0300

    trocando Link por link nativo

--- app/views/admin/products/index.rb
+++ app/views/admin/products/index.rb
@@ -18,10 +18,11 @@ class Views::Admin::Products::Index < Views::Base
         "Produtos"
       end
 
-      Link href: new_admin_product_path,
-           class: "inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" do
-        icon "plus", class: "w-4 h-4 mr-2"
-        "Novo Produto"
+      link_to new_admin_product_path, class: "inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" do
+        icon "plus", class: "w-4 h-4 mr-2 flex-shrink-0"
+        span class: "whitespace-nowrap" do
+          "Novo Produto"
+        end
       end
     end
   end
