ba733b57b5588a800f9e1a8f45b26f68a0a5f8dc
Elisson17 <elissoncordeiro2004@gmail.com>
2025-08-31 07:12:39 -0300

    style/ Melhorando tela de registro

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :name, presence: { message: "Nome é obrigatório" }
+  validates :email, presence: { message: "E-mail é obrigatório" },
+                   uniqueness: { message: "Este e-mail já está sendo usado por outra conta" }
 
   def admin?
     role == "admin"
